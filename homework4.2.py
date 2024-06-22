1336c47cbfe463c96915035efef1466d3c2efd3c
Dmitry-S85 <dmitry.stoyankin@yandex.ru>
2024-06-22 17:24:10 +0300

    Add files via upload

---
+++ homework4.2.py
@@ -0,0 +1,16 @@
+my_string = 'Создана строка, для выполнения домашнего задания'
+
+#Вывод строки my_string в верхнем регистре.
+print(my_string.upper())
+
+#Вывод строк my_string в нижнем регистре.
+print(my_string.lower())
+
+#Изменение строки my_string, удаление всех пробелов.
+print(my_string.replace(' ', ''))
+
+#Вывод первого символа строки my_string.
+print(my_string[0])
+
+#Вывод последнего символа строки my_string.
+print(my_string[-1])
